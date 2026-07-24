268747c98cf8642cde157d3f5b64b9402f128c4c
Snigdha Maiti <snigdhamaiti2207@gmail.com>
2026-07-24 17:11:52 +0530

    Restriction enzyme site detection

---
+++ restriction-enzyme-site-finder/enzymes.py
@@ -0,0 +1,11 @@
+
+restriction_enzymes = {
+    "EcoRI": "GAATTC",
+    "BamHI": "GGATCC",
+    "HindIII": "AAGCTT",
+    "NotI": "GCGGCCGC",
+    "XhoI": "CTCGAG",
+    "PstI": "CTGCAG",
+    "SmaI": "CCCGGG",
+    "KpnI": "GGTACC"
+}
